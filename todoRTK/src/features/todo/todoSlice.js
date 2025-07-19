6c39716b9faede813841b76cd6daef0c2b7fb0cb
Umergul13075 <145903053+Umergul13075@users.noreply.github.com>
2025-07-19 15:39:11 +0500

    first commit

---
+++ todoRTK/src/features/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit' 
+
+const initialState = {
+    todos:[{
+        id:1,
+        text:"Hello World",
+    }]
+}
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo:(state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload.text,
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo:(state, action)=>{
+            const {id, newText} = action.payload;
+            const todoToUpdate = state.todos.find(todo => todo.id ===id);
+            if(todoToUpdate){
+                todoToUpdate.text = newText;
+            }
+        }  
+    },
+})
+
+export const {addTodo, updateTodo, removeTodo} = todoSlice.actions;
+
+export default todoSlice.reducer
